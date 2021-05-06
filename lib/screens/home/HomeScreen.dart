5b2596343eaabf3f79e9bb9634164dc172001c2b
Muthu Thavamani <muthucs@outlook.com>
2021-05-06 20:07:58 +0530

    ui cleanups

--- lib/screens/home/HomeScreen.dart
+++ lib/screens/home/HomeScreen.dart
@@ -31,7 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = Size((MediaQuery.of(context).size.width / 5), 100);
+    Size size = Size((MediaQuery.of(context).size.width / 2), 100);
 
     return WillPopScope(
       onWillPop: onWillPop,
@@ -40,69 +40,7 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: CustomColors.lightGrey,
         body: SingleChildScrollView(
           child: _selectedIndex == 0
-              ? Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Column(children: [
-                          ListTile(
-                            title: Text(
-                              "Buy anything from your Local Stores",
-                              style: TextStyle(
-                                  color: CustomColors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 15),
-                            ),
-                          ),
-                          InkWell(
-                            onTap: () {
-                              // Navigator.push(
-                              //   context,
-                              //   MaterialPageRoute(
-                              //     builder: (context) => SearchAppBar(3, ''),
-                              //     settings:
-                              //         RouteSettings(name: '/search/categories'),
-                              //   ),
-                              // );
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 8, bottom: 8),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.grid_view,
-                                    size: 15,
-                                    color: Colors.black,
-                                  ),
-                                  SizedBox(width: 3),
-                                  Text(
-                                    "More Categories",
-                                    style: TextStyle(
-                                        decoration: TextDecoration.underline,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 10),
-                                  ),
-                                  SizedBox(width: 3),
-                                  Icon(
-                                    Icons.keyboard_arrow_right,
-                                    size: 18,
-                                    color: Colors.grey,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ]),
-                      ),
-                    ),
-                  ],
-                )
+              ? Container()
               : SettingsHome(),
         ),
         bottomNavigationBar: Container(
@@ -131,35 +69,6 @@ class _HomeScreenState extends State<HomeScreen> {
                         color: CustomColors.black,
                       ),
                       Text("Home", style: GoogleFonts.orienta()),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox.fromSize(
-                size: size,
-                child: InkWell(
-                  onTap: () {
-                    _onItemTapped(1);
-                  },
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.all(3.0),
-                        decoration: BoxDecoration(
-                          color: CustomColors.black,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Icon(
-                          Icons.search,
-                          size: 16.0,
-                          color: CustomColors.white,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 3,
-                      ),
-                      Text("Search", style: GoogleFonts.orienta()),
                     ],
                   ),
                 ),
