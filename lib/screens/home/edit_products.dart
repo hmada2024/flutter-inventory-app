515cc1c57b6f441a1378d2f637df44adbde59979
JeevanivasanRamasamy <65586291+JeevanivasanRamasamy@users.noreply.github.com>
2021-05-10 13:39:35 +0530

    Update edit_products.dart
    Added the validation "Quantity should greater than alert quantity"

--- lib/screens/home/edit_products.dart
+++ lib/screens/home/edit_products.dart
@@ -31,160 +31,152 @@ class _EditProductState extends State<EditProduct> {
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Text("Edit Product"),
-        leading: InkWell(
-          child: Icon(Icons.arrow_back_ios),
+      body: AlertDialog(
+        title: GestureDetector(
           onTap: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: GestureDetector(
-        onTap: () {
-          FocusScopeNode currentFocus = FocusScope.of(context);
+            FocusScopeNode currentFocus = FocusScope.of(context);
 
-          if (!currentFocus.hasPrimaryFocus) {
-            currentFocus.unfocus();
-          }
-        },
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: SafeArea(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 10,
-                          left: 20,
-                          right: 20,
-                        ),
-                        child: Column(
-                          children: [
-                            ListTile(
-                              leading: Icon(
-                                Icons.store,
-                                size: 35,
-                                color: CustomColors.black,
-                              ),
-                              title: Container(
-                                child: TextFormField(
-                                  initialValue: widget.product.businessName,
-                                  textAlign: TextAlign.end,
-                                  autofocus: false,
-                                  readOnly: true,
-                                  decoration: InputDecoration(
-                                    fillColor: CustomColors.white,
-                                    filled: true,
-                                    contentPadding: EdgeInsets.symmetric(
-                                        vertical: 1.0, horizontal: 5.0),
-                                    border: OutlineInputBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(10.0),
-                                        borderSide: BorderSide(
-                                            color: CustomColors.grey)),
+            if (!currentFocus.hasPrimaryFocus) {
+              currentFocus.unfocus();
+            }
+          },
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: SafeArea(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 10,
+                            left: 20,
+                            right: 20,
+                          ),
+                          child: Column(
+                            children: [
+                              ListTile(
+                                leading: Icon(
+                                  Icons.store,
+                                  size: 35,
+                                  color: CustomColors.black,
+                                ),
+                                title: Container(
+                                  child: TextFormField(
+                                    initialValue: widget.product.businessName,
+                                    textAlign: TextAlign.end,
+                                    autofocus: false,
+                                    readOnly: true,
+                                    decoration: InputDecoration(
+                                      fillColor: CustomColors.white,
+                                      filled: true,
+                                      contentPadding: EdgeInsets.symmetric(
+                                          vertical: 1.0, horizontal: 5.0),
+                                      border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                          borderSide: BorderSide(
+                                              color: CustomColors.grey)),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            Container(
-                              margin:
-                                  const EdgeInsets.only(top: 5, bottom: 5),
-                              child: TextFormField(
-                                initialValue: widget.product.name,
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(12.0)),
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                      borderSide: const BorderSide(
-                                          color: Colors.grey, width: 2.0),
-                                    ),
-                                    labelText: 'Name',
-                                    hintText: 'Enter your name'),
-                                validator: (name) {
-                                  if (name.isEmpty) {
-                                    return "Must not be empty";
-                                  } else {
-                                    this.pName = name;
+                              Container(
+                                margin:
+                                    const EdgeInsets.only(top: 5, bottom: 5),
+                                child: TextFormField(
+                                  initialValue: widget.product.name,
+                                  decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(12.0)),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderSide: const BorderSide(
+                                            color: Colors.grey, width: 2.0),
+                                      ),
+                                      labelText: 'Name',
+                                      hintText: 'Enter your name'),
+                                  validator: (name) {
+                                    if (name.isEmpty) {
+                                      return "Must not be empty";
+                                    } else {
+                                      this.pName = name;
+                                      return null;
+                                    }
+                                  },
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.only(top: 5, bottom: 5),
+                                child: TextFormField(
+                                  initialValue:
+                                      widget.product.quantity.toString(),
+                                  decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(12.0)),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderSide: const BorderSide(
+                                            color: Colors.grey, width: 2.0),
+                                      ),
+                                      labelText: 'Quantity',
+                                      hintText: 'Available Quantity'),
+                                  validator: (quantity) {
+                                    if (quantity.isEmpty) {
+                                      qty = double.parse('0');
+                                    } else {
+                                      qty = double.parse(quantity);
+                                    }
                                     return null;
-                                  }
+                                  },
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.only(top: 5, bottom: 5),
+                                child: TextFormField(
+                                  initialValue:
+                                      widget.product.minQuantity.toString(),
+                                  decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(12.0)),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderSide: const BorderSide(
+                                            color: Colors.grey, width: 2.0),
+                                      ),
+                                      labelText: 'Alert Quantity',
+                                      hintText: 'Alert Quantity'),
+                                  validator: (quantity) {
+                                    if (quantity.isEmpty) {
+                                      minQty = double.parse('0');
+                                    } else {
+                                      minQty = double.parse(quantity);
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                              ElevatedButton(
+                                child: Text('Update'),
+                                onPressed: () async {
+                                  await _submit();
                                 },
                               ),
-                            ),
-                            Container(
-                              margin:
-                                  const EdgeInsets.only(top: 5, bottom: 5),
-                              child: TextFormField(
-                                initialValue:
-                                    widget.product.quantity.toString(),
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(12.0)),
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                      borderSide: const BorderSide(
-                                          color: Colors.grey, width: 2.0),
-                                    ),
-                                    labelText: 'Quantity',
-                                    hintText: 'Available Quantity'),
-                                validator: (quantity) {
-                                  if (quantity.isEmpty) {
-                                    qty = double.parse('0');
-                                  } else {
-                                    qty = double.parse(quantity);
-                                  }
-                                  return null;
-                                },
-                              ),
-                            ),
-                            Container(
-                              margin:
-                                  const EdgeInsets.only(top: 5, bottom: 5),
-                              child: TextFormField(
-                                initialValue:
-                                    widget.product.minQuantity.toString(),
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(12.0)),
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                      borderSide: const BorderSide(
-                                          color: Colors.grey, width: 2.0),
-                                    ),
-                                    labelText: 'Min Quantity',
-                                    hintText: 'Alert Quantity'),
-                                validator: (quantity) {
-                                  if (quantity.isEmpty) {
-                                    minQty = double.parse('0');
-                                  } else {
-                                    minQty = double.parse(quantity);
-                                  }
-                                  return null;
-                                },
-                              ),
-                            ),
-                            SizedBox(height: 20),
-                            ElevatedButton(
-                              child: Text('Update'),
-                              onPressed: () async {
-                                await _submit();
-                              },
-                            ),
-                            SizedBox(height: 20)
-                          ],
+                              SizedBox(height: 20)
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -209,8 +201,15 @@ class _EditProductState extends State<EditProduct> {
             this.pName.split(" ").map((e) => e.toLowerCase()).toList();
 
         // CustomDialogs.actionWaiting(context);
-        await _p.update(_p.toJson());
-        Navigator.pop(context);
+        if (qty > minQty) {
+          await _p.update(_p.toJson());
+          Navigator.pop(context);
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+              CustomSnackBar.errorSnackBar(
+                  "Quantity should greater than alert quantity", 2));
+        }
+
         // Navigator.pop(context);
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
